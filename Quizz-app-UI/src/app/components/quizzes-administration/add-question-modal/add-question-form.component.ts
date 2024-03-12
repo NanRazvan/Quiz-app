a76afdac1dc7d45e4cf2f737e8a6b8c8e6ae52f7
Nan Cosmin Răzvan <nancosminrazvan@gmail.com>
2024-03-12 14:27:41 +0200

    [Add quesions] button functionality added

---
+++ Quizz-app-UI/src/app/components/quizzes-administration/add-question-modal/add-question-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-question-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-question-form.component.html',
+  styleUrl: './add-question-form.component.css'
+})
+export class AddQuestionFormComponent {
+
+}
